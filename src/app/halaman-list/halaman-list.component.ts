ce595b89333229f1ea654174d6a73fac3466cad4
wen4752 <wenaudi74@gmail.com>
2020-04-22 11:43:50 +0700

    2 files modified

--- src/app/halaman-list/halaman-list.component.ts
+++ src/app/halaman-list/halaman-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-halaman-list',
@@ -7,9 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HalamanListComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private route :ActivatedRoute) { }
+  hasil = [{}];
   ngOnInit() {
+    let list = this.route.snapshot.paramMap.get('listJurusan');
+    
   }
 
 }
