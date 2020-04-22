b692b235b4fcbf2dce962ad2cdc5d2d87cde8966
wen4752 <wenaudi74@gmail.com>
2020-04-22 10:52:21 +0700

    2 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,24 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+
+import {Routes,RouterModule} from '@angular/router';
 import { HalamanInputComponent } from './halaman-input/halaman-input.component';
 import { HalamanListComponent } from './halaman-list/halaman-list.component';
 import { HalamanEditComponent } from './halaman-edit/halaman-edit.component';
+import halamanInputComponentHtml from './halaman-input/halaman-input.component.html';
 
+const ROUTES:Routes = [
+  {path:'halaman-input',component:HalamanInputComponent},
+  {path:'halaman-edit',component: HalamanEditComponent},
+  {path:'halaman-list',component:HalamanListComponent}
+]
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ 
+    BrowserModule, 
+    FormsModule,
+    RouterModule.forRoot(ROUTES) 
+    ],
   declarations: [ AppComponent, HelloComponent, HalamanInputComponent, HalamanListComponent, HalamanEditComponent ],
   bootstrap:    [ AppComponent ]
 })
